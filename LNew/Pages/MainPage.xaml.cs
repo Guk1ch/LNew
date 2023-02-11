7496a160a78483dd9daa37c5038a13ad24432c41
201903 <201903@TOCHKA-008>
2023-02-11 14:17:49 +0300

    bcvbf

--- LNew/Pages/MainPage.xaml.cs
+++ LNew/Pages/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LNew.DataBase;
 
 namespace LNew.Pages
 {
@@ -20,14 +21,41 @@ namespace LNew.Pages
     /// </summary>
     public partial class MainPage : Page
     {
+        public List<Product> prod { get; set; }
+        public List<ProductType> types { get; set; }
+        public List<Sorting> sortings { get; set; }
         public MainPage()
         {
             InitializeComponent();
+            prod = BdConnection.connection.Product.ToList();
+            types =BdConnection.connection.ProductType.ToList();
+            types.Insert(0, new ProductType {  Id = 0, Name = "Все типы"});
+            cbFiltr.ItemsSource = types;
+            cbFiltr.DisplayMemberPath = "Name";
+
+            sortings = new List<Sorting>()
+            {
+                new Sorting() {Id=0, Name ="Пусто"},
+                new Sorting() {Id=1, Name="Минимальная стоимость по убыванию"},
+                new Sorting() {Id=2, Name="Минимальная стоимость апо возростанию"},
+                new Sorting() {Id=3, Name="Наименование по убыванию"},
+                new Sorting() {Id=4, Name="Наименование по возростанию"},
+                new Sorting() {Id=5, Name="Номер цеха по убыванию"},
+                new Sorting() {Id=6, Name="Номер цеха по возростанию"},
+            };
+            cbSort.ItemsSource = sortings;
+            cbSort.DisplayMemberPath = "Name";
+            this.DataContext = this;
         }
 
         private void lvProd_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+        public class Sorting
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
         }
 
         private void btnADD_Click(object sender, RoutedEventArgs e)
